a51f6e7139c82cb7e06764443ea69e799e03807f
Windyy-1007 <namphongtip02@gmail.com>
2024-07-19 07:57:31 +0700

    Fix error on moveAllowed

--- botplayer.py
+++ botplayer.py
@@ -1,77 +1,147 @@
 import runner
 import math
 
-def minimax(str, depth, player):
-    curBoard = runner.Board(str)
-    optimal_action_x = 0
-    optimal_action_y = 0
-    optimal_direction = 'L'
-    
-    if depth == 0 or curBoard.endGame():
-        curBoard.movePiece(optimal_action_x, optimal_action_y, optimal_direction, player)
-        return curBoard.getString()
-    if player == 1:
-        bestVal = -math.inf
-        for i in range(8):
-            for j in range(8):
-                if curBoard.movePiece(i, j, 1):
-                    value = minimax(evaluate(curBoard), depth - 1, -1)
-                    bestVal = max(bestVal, value)
-                    optimal_action_x = i
-                    optimal_action_y = j
-                    optimal_direction = 'R'
-        curBoard.movePiece(optimal_action_x, optimal_action_y,optimal_direction, 1)
-        return curBoard.getString()
-    else:
-        bestVal = math.inf
-        for i in range(8):
-            for j in range(8):
-                if curBoard.movePiece(i, j, 'L', -1):
-                    value = minimax(evaluate(curBoard), depth - 1, 1)
-                    bestVal = min(bestVal, value)
-                    optimal_action_x = i
-                    optimal_action_y = j
-                    optimal_direction = 'L'
-                if curBoard.movePiece(i, j, 'R', -1):
-                    value = minimax(evaluate(curBoard), depth - 1, 1)
-                    bestVal = min(bestVal, value)
-                    optimal_action_x = i
-                    optimal_action_y = j
-                    optimal_direction = 'R'
-        curBoard.movePiece(optimal_action_x, optimal_action_y,optimal_direction, -1)
-        return curBoard.getString()
+def min_value(str, depth):
+    tempBoard = runner.Board(str)
+    backupBoard = runner.Board(str)
+    if depth == 0:
+        print (evaluate(str))
+        return evaluate(str)
+    if tempBoard.endGame():
+        return tempBoard.utility()
+    v = math.inf
+    for i in range(8):
+        for j in range(8):
+            if tempBoard.board[i][j] == 0:
+                continue
+            if tempBoard.moveAllowed(i, j, 'L', -1):
+                tempBoard.move(i, j, 'L', -1)
+                v = min(v, max_value(tempBoard.getString(), depth - 1))
+                tempBoard.board = backupBoard.board
+            if tempBoard.moveAllowed(i, j, 'R', -1):
+                tempBoard.move(i, j, 'R', -1)
+                v = min(v, max_value(tempBoard.getString(), depth - 1))
+                tempBoard.board = backupBoard.board
+            if tempBoard.moveAllowed(i, j, '-L', -1):
+                tempBoard.move(i, j, '-L', -1)
+                v = min(v, max_value(tempBoard.getString(), depth - 1))
+                tempBoard.board = backupBoard.board
+            if tempBoard.moveAllowed(i, j, '-R', -1):
+                tempBoard.move(i, j, '-R', -1)
+                v = min(v, max_value(tempBoard.getString(), depth - 1))
+                tempBoard.board = backupBoard.board
+    return v
 
-def evaluate(str):
-    board = runner.Board(str)
-    scoreW = 0
-    scoreB = 0
+def max_value(str, depth):
+    tempBoard = runner.Board(str)
+    backupBoard = runner.Board(str)
+    if depth == 0:
+        print (evaluate(str))
+        return evaluate(str)
+    if tempBoard.endGame():
+        return tempBoard.utility()
+    v = -math.inf
+    for i in range(8):
+        for j in range(8):
+            if tempBoard.board[i][j] == 0:
+                continue
+            if tempBoard.moveAllowed(i, j, 'L', 1):
+                tempBoard.move(i, j, 'L', 1)
+                v = max(v, min_value(tempBoard.getString(), depth - 1))
+                tempBoard.board = backupBoard.board
+            if tempBoard.moveAllowed(i, j, 'R', 1):
+                tempBoard.move(i, j, 'R', 1)
+                v = max(v, min_value(tempBoard.getString(), depth - 1))
+                tempBoard.board = backupBoard.board
+            if tempBoard.moveAllowed(i, j, '-L', 1):
+                tempBoard.move(i, j, '-L', 1)
+                v = max(v, min_value(tempBoard.getString(), depth - 1))
+                tempBoard.board = backupBoard.board
+            if tempBoard.moveAllowed(i, j, '-R', 1):
+                tempBoard.move(i, j, '-R', 1)
+                v = max(v, min_value(tempBoard.getString(), depth - 1))
+                tempBoard.board = backupBoard.board
+    return v
+                    
+def minimax(str, depth, turn):
+    tempBoard = runner.Board(str)
+    backupBoard = runner.Board(str)
+    optimalMove = 'N'
+    if turn == 1:
+        v = -math.inf
+        for i in range(8):
+            for j in range(8):
+                if tempBoard.board[i][j] == 0:
+                    continue
+                if tempBoard.moveAllowed(i, j, 'L', 1):
+                    tempBoard.move(i, j, 'L', 1)
+                    if(min_value(tempBoard.getString(), depth - 1) > v):
+                        v = min_value(tempBoard.getString(), depth - 1)
+                        optimalMove = tempBoard.getString()
+                    tempBoard.board = backupBoard.board
+                if tempBoard.moveAllowed(i, j, 'R', 1):
+                    tempBoard.move(i, j, 'R', 1)
+                    if(min_value(tempBoard.getString(), depth - 1) > v):
+                        v = min_value(tempBoard.getString(), depth - 1)
+                        optimalMove = tempBoard.getString()
+                    tempBoard.board = backupBoard.board
+                if tempBoard.moveAllowed(i, j, '-L', 1):
+                    tempBoard.move(i, j, '-L', 1)
+                    if(min_value(tempBoard.getString(), depth - 1) > v):
+                        v = min_value(tempBoard.getString(), depth - 1)
+                        optimalMove = tempBoard.getString()
+                    tempBoard.board = backupBoard.board
+                if tempBoard.moveAllowed(i, j, '-R', 1):
+                    tempBoard.move(i, j, '-R', 1)
+                    if(min_value(tempBoard.getString(), depth - 1) > v):
+                        v = min_value(tempBoard.getString(), depth - 1)
+                        optimalMove = tempBoard.getString()
+                    tempBoard.board = backupBoard.board
+    else:
+        v = math.inf
+        for i in range(8):
+            for j in range(8):
+                if tempBoard.board[i][j] == 0:
+                    continue
+                if tempBoard.moveAllowed(i, j, 'L', -1):
+                    tempBoard.move(i, j, 'L', -1)
+                    if(max_value(tempBoard.getString(), depth - 1) < v):
+                        v = max_value(tempBoard.getString(), depth - 1)
+                        optimalMove = tempBoard.getString()
+                    tempBoard.board = backupBoard.board
+                if tempBoard.moveAllowed(i, j, 'R', -1):
+                    tempBoard.move(i, j, 'R', -1)
+                    if(max_value(tempBoard.getString(), depth - 1) < v):
+                        v = max_value(tempBoard.getString(), depth - 1)
+                        optimalMove = tempBoard.getString()
+                    tempBoard.board = backupBoard.board
+                if tempBoard.moveAllowed(i, j, '-L', -1):
+                    tempBoard.move(i, j, '-L', -1)
+                    if(max_value(tempBoard.getString(), depth - 1) < v):
+                        v = max_value(tempBoard.getString(), depth - 1)
+                        optimalMove = tempBoard.getString()
+                    tempBoard.board = backupBoard.board
+                if tempBoard.moveAllowed(i, j, '-R', -1):
+                    tempBoard.move(i, j, '-R', -1)
+                    if(max_value(tempBoard.getString(), depth - 1) < v):
+                        v = max_value(tempBoard.getString(), depth - 1)
+                        optimalMove = tempBoard.getString()
+                    tempBoard.board = backupBoard.board
+    print (v)
+    print ('\n')
+    return optimalMove
+
+def evaluate(string):
+    board = runner.Board(string)
+    scoreW = 0.000
+    scoreB = 0.000
     for i in range(8):
         for j in range(8):
             if board.board[i][j] > 0:
                 scoreW += board.board[i][j]
             elif board.board[i][j] < 0:
                 scoreB += board.board[i][j]
-    return (1000 * scoreW) / (1000 * scoreB)
+    print (scoreW / (scoreB + scoreW))
+    return (scoreW) / (scoreB + scoreW)
             
-def min_value(str, depth):
-    board = runner.Board(str)
-    if board.endGame():
-        return board.utility()
-    v = math.inf
-    for i in range(8):
-        for j in range(8):
-            if board.movePiece(i, j, -1):
-                v = min(v, max_value(evaluate(board)))
-    return v
-
-def max_value(str, depth):
-    board = runner.Board(str)
-    if board.endGame():
-        return board.utility()
-    v = -math.inf
-    for i in range(8):
-        for j in range(8):
-            if board.movePiece(i, j, 1):
-                v = max(v, min_value(evaluate(board)))
-    return v
-
+evaluate('0b0b0b0bb0b0b0b00b0b0b0b0000000000000000w0w0w0w00w0w0w0ww0w0w0w0')
--- runner.py
+++ runner.py
@@ -1,17 +1,13 @@
 # Main file
 
 import sys
-import os
-import botplayer as bp
+import math
 
 # Board and pieces
 intialPos = '0b0b0b0bb0b0b0b00b0b0b0b0000000000000000w0w0w0w00w0w0w0ww0w0w0w0'
 winner = 0
 
 class Board:
-    def __init__(self):
-        self.board = [[0 for i in range(8)] for j in range(8)]
-        
     def __init__(self, str):
         self.board = [[0 for i in range(8)] for j in range(8)]
         for i in range(63):
@@ -183,7 +179,139 @@ class Board:
                         if i + 2 < 8 and j + 2 < 8 and self.board[i+1][j+1] == 2 and self.board[i+2][j+2] == 0:
                             return True
     
-    def movePiece(self, x, y, direction, turn):
+    def moveAllowed(self, x, y, direction, turn):
+        if (self.board[x][y] == 0):
+            return False
+        if (turn == 1 and self.board[x][y] == -1):
+            return False
+        if (turn == -1 and self.board[x][y] == 1):
+            return False
+        if turn == 1:
+            if (direction == 'L'):
+                if(y == 0 or x == 0):
+                    return False
+                if (self.board[x-1][y-1] == 1):
+                    return False
+                if (self.board[x-1][y-1] == -1):
+                    if(x == 1 or y == 1):
+                        return False
+                    if(self.board[x-2][y-2] != 0):
+                        return False
+                    return True
+                if (self.board[x-1][y-1] == 0):
+                    if(self.availableCapture(turn)):
+                        return False
+                    return True
+            if (direction == 'R'):
+                if(y == 7 or x == 0):
+                    return False
+                if (self.board[x-1][y+1] == 1):
+                    return False
+                if (self.board[x-1][y+1] == -1):
+                    if(x == 1 or y == 6):
+                        return False
+                    if(self.board[x-2][y+2] != 0):
+                        return False
+                    return True
+                if (self.board[x-1][y+1] == 0):
+                    if(self.availableCapture(turn)):
+                        return False
+                    return True
+            if (direction == '-L'):
+                if(self.board[x][y] != 2):
+                    return False
+                if(y == 0 or x == 7):
+                    return False
+                if (self.board[x+1][y-1] == -1):
+                    return False
+                if (self.board[x+1][y-1] == 1):
+                    if(x == 6 or y == 1):
+                        return False
+                    if(self.board[x+2][y-2] != 0):
+                        return False
+                    return True
+                if (self.board[x+1][y-1] == 0):
+                    if(self.availableCapture(turn)):
+                        return False
+                    return True
+            if (direction == '-R'):
+                if(self.board[x][y] != 2):
+                    return False
+                if(y == 7 or x == 7):
+                    return False
+                if (self.board[x+1][y+1] == -1):
+                    return False
+                if (self.board[x+1][y+1] == 1):
+                    if(x == 6 or y == 6):
+                        return False
+                    if(self.board[x+2][y+2] != 0):
+                        return False
+                    return True
+                if (self.board[x+1][y+1] == 0):
+                    if(self.availableCapture(turn)):
+                        return False
+                    return True
+        if turn == -1:
+            if (direction == 'L'):
+                if(y == 0 or x == 7):
+                    return False
+                if (self.board[x+1][y-1] == -1):
+                    return False
+                if (self.board[x+1][y-1] == 1):
+                    if(x == 6 or y == 1):
+                        return False
+                    if(self.board[x+2][y-2] != 0):
+                        return False
+                    return True
+                if (self.board[x+1][y-1] == 0):
+                    if(self.availableCapture(turn)):
+                        return False
+                    return True
+            if (direction == 'R'):
+                if(y == 7 or x == 7):
+                    return False
+                if (self.board[x+1][y+1] == -1):
+                    return False
+                if (self.board[x+1][y+1] == 1):
+                    if(x == 6 or y == 6):
+                        return False
+                    if(self.board[x+2][y+2] != 0):
+                        return False
+                    return True
+                if (self.board[x+1][y+1] == 0):
+                    if(self.availableCapture(turn)):
+                        return False
+                    return True
+            if (direction == '-L'):
+                if(self.board[x][y] != -2):
+                    return False
+                if(y == 0 or x == 0):
+                    return False
+                if (self.board[x-1][y-1] == 1):
+                    return False
+                if (self.board[x-1][y-1] == -1):
+                    if(x == 1 or y == 1):
+                        return False
+                    if(self.board[x-2][y-2] != 0):
+                        return False
+                    return True
+                if (self.board[x-1][y-1] == 0):
+                    if(self.availableCapture(turn)):
+                        return False
+                    return True
+            if (direction == '-R'):
+                if(self.board[x][y] != -2):
+                    return False
+                if(y == 7 or x == 0):
+                    return False
+                if (self.board[x-1][y+1] == 1):
+                    return False
+                if (self.board[x-1][y+1] == -1):
+                    if(x == 1 or y == 6):
+                        return False
+        return False
+    
+    def move(self, x, y, direction, turn):
         if (self.board[x][y] == 0):
             return False
         if (turn == 1 and self.board[x][y] == -1):
@@ -362,7 +490,150 @@ class Board:
                     self.board[x][y] = 0
                     return True
         return False
-    
+
+#AI
+
+def min_value(str, depth = 10):
+    tempBoard = Board(str)
+    backupBoard = Board(str)
+    if depth == 0:
+        return evaluate(str)
+    if tempBoard.endGame():
+        return tempBoard.utility()
+    v = math.inf
+    for i in range(8):
+        for j in range(8):
+            if tempBoard.board[i][j] == 0:
+                continue
+            if tempBoard.moveAllowed(i, j, 'L', -1):
+                tempBoard.move(i, j, 'L', -1)
+                v = min(v, max_value(tempBoard.getString(), depth - 1))
+                tempBoard.board = backupBoard.board
+            if tempBoard.moveAllowed(i, j, 'R', -1):
+                tempBoard.move(i, j, 'R', -1)
+                v = min(v, max_value(tempBoard.getString(), depth - 1))
+                tempBoard.board = backupBoard.board
+            if tempBoard.moveAllowed(i, j, '-L', -1):
+                tempBoard.move(i, j, '-L', -1)
+                v = min(v, max_value(tempBoard.getString(), depth - 1))
+                tempBoard.board = backupBoard.board
+            if tempBoard.moveAllowed(i, j, '-R', -1):
+                tempBoard.move(i, j, '-R', -1)
+                v = min(v, max_value(tempBoard.getString(), depth - 1))
+                tempBoard.board = backupBoard.board
+    return v
+
+def max_value(str, depth = 10):
+    tempBoard = Board(str)
+    backupBoard = Board(str)
+    if depth == 0:
+        return evaluate(str)
+    if tempBoard.endGame():
+        return tempBoard.utility()
+    v = -math.inf
+    for i in range(8):
+        for j in range(8):
+            if tempBoard.board[i][j] == 0:
+                continue
+            if tempBoard.moveAllowed(i, j, 'L', 1):
+                tempBoard.move(i, j, 'L', 1)
+                v = max(v, min_value(tempBoard.getString(), depth - 1))
+                tempBoard.board = backupBoard.board
+            if tempBoard.moveAllowed(i, j, 'R', 1):
+                tempBoard.move(i, j, 'R', 1)
+                v = max(v, min_value(tempBoard.getString(), depth - 1))
+                tempBoard.board = backupBoard.board
+            if tempBoard.moveAllowed(i, j, '-L', 1):
+                tempBoard.move(i, j, '-L', 1)
+                v = max(v, min_value(tempBoard.getString(), depth - 1))
+                tempBoard.board = backupBoard.board
+            if tempBoard.moveAllowed(i, j, '-R', 1):
+                tempBoard.move(i, j, '-R', 1)
+                v = max(v, min_value(tempBoard.getString(), depth - 1))
+                tempBoard.board = backupBoard.board
+    return v
+                    
+def minimax(str, depth, turn):
+    tempBoard = Board(str)
+    backupBoard = Board(str)
+    optimalMove = 'N'
+    if turn == 1:
+        v = -math.inf
+        for i in range(8):
+            for j in range(8):
+                if tempBoard.board[i][j] == 0:
+                    continue
+                if tempBoard.moveAllowed(i, j, 'L', 1):
+                    tempBoard.move(i, j, 'L', 1)
+                    if(min_value(tempBoard.getString(), depth - 1) > v):
+                        v = min_value(tempBoard.getString(), depth - 1)
+                        optimalMove = tempBoard.getString()
+                    tempBoard.board = backupBoard.board
+                if tempBoard.moveAllowed(i, j, 'R', 1):
+                    tempBoard.move(i, j, 'R', 1)
+                    if(min_value(tempBoard.getString(), depth - 1) > v):
+                        v = min_value(tempBoard.getString(), depth - 1)
+                        optimalMove = tempBoard.getString()
+                    tempBoard.board = backupBoard.board
+                if tempBoard.moveAllowed(i, j, '-L', 1):
+                    tempBoard.move(i, j, '-L', 1)
+                    if(min_value(tempBoard.getString(), depth - 1) > v):
+                        v = min_value(tempBoard.getString(), depth - 1)
+                        optimalMove = tempBoard.getString()
+                    tempBoard.board = backupBoard.board
+                if tempBoard.moveAllowed(i, j, '-R', 1):
+                    tempBoard.move(i, j, '-R', 1)
+                    if(min_value(tempBoard.getString(), depth - 1) > v):
+                        v = min_value(tempBoard.getString(), depth - 1)
+                        optimalMove = tempBoard.getString()
+                    tempBoard.board = backupBoard.board
+    else:
+        v = math.inf
+        for i in range(8):
+            for j in range(8):
+                if tempBoard.board[i][j] == 0:
+                    continue
+                if tempBoard.moveAllowed(i, j, 'L', -1):
+                    tempBoard.move(i, j, 'L', -1)
+                    if(max_value(tempBoard.getString(), depth - 1) < v):
+                        v = max_value(tempBoard.getString(), depth - 1)
+                        optimalMove = tempBoard.getString()
+                    tempBoard.board = backupBoard.board
+                if tempBoard.moveAllowed(i, j, 'R', -1):
+                    tempBoard.move(i, j, 'R', -1)
+                    if(max_value(tempBoard.getString(), depth - 1) < v):
+                        v = max_value(tempBoard.getString(), depth - 1)
+                        optimalMove = tempBoard.getString()
+                    tempBoard.board = backupBoard.board
+                if tempBoard.moveAllowed(i, j, '-L', -1):
+                    tempBoard.move(i, j, '-L', -1)
+                    if(max_value(tempBoard.getString(), depth - 1) < v):
+                        v = max_value(tempBoard.getString(), depth - 1)
+                        optimalMove = tempBoard.getString()
+                    tempBoard.board = backupBoard.board
+                if tempBoard.moveAllowed(i, j, '-R', -1):
+                    tempBoard.move(i, j, '-R', -1)
+                    if(max_value(tempBoard.getString(), depth - 1) < v):
+                        v = max_value(tempBoard.getString(), depth - 1)
+                        optimalMove = tempBoard.getString()
+                    tempBoard.board = backupBoard.board
+    print ('v' ,v)
+    return optimalMove
+
+def evaluate(string):
+    board = Board(string)
+    scoreW = 0.000
+    scoreB = 0.000
+    for i in range(8):
+        for j in range(8):
+            if board.board[i][j] > 0:
+                scoreW += board.board[i][j]
+            elif board.board[i][j] < 0:
+                scoreB += -board.board[i][j]
+    return (scoreW) / (scoreB + scoreW)
+            
+evaluate('0b0b0b0bb0b0b0b00b0b0b0b0000000000000000w0w0w0w00w0w0w0ww0w0w0w0')
+
 # play a game
 def playGame():
     board = Board(intialPos)
@@ -373,7 +644,7 @@ def playGame():
         x = int(input("Enter x coordinate: "))
         y = int(input("Enter y coordinate: "))
         direction = input("Enter direction: ")
-        if board.movePiece(x, y, direction, turn):
+        if board.move(x, y, direction, turn):
             board.printBoard()
             turn = -turn
         else:
@@ -393,16 +664,17 @@ def playGameBot():
             x = int(input("Enter x coordinate: "))
             y = int(input("Enter y coordinate: "))
             direction = input("Enter direction: ")
-            if board.movePiece(x, y, direction, turn):
+            if board.move(x, y, direction, turn):
                 board.printBoard()
                 turn = -turn
             else:
                 print("Invalid move")
         else:
             curPos = board.getString()
-            minimaxPos = bp.minimax(curPos, 5, turn)
-            
-            
+            suggestedPos = minimax(curPos, 5, -1)
+            board.editBoard(suggestedPos)
+            board.printBoard()
+            turn = -turn
     if winner == 1:
         print("Player 1 wins")
     else:
